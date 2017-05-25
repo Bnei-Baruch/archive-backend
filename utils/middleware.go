f2b669abc46aab649ea7f61e304d6cc7f5676eab
Edo Shor <edoshor@gmail.com>
2017-05-25 12:17:52 +0300

    Middleware - log errors to rollbar

--- utils/middleware.go
+++ utils/middleware.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"runtime"
 	"runtime/debug"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
@@ -74,6 +76,66 @@ func BindErrorMessage(err error) string {
 	}
 }
 
+type stackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+func errorsToRollbarStack(st stackTracer) rollbar.Stack {
+	t := st.StackTrace()
+	rs := make(rollbar.Stack, len(t))
+	for i, f := range t {
+		// Program counter as it's computed internally in errors.Frame
+		pc := uintptr(f) - 1
+		fn := runtime.FuncForPC(pc)
+		if fn == nil {
+			rs[i] = rollbar.Frame{
+				Filename: "unknown",
+				Method:   "?",
+				Line:     0,
+			}
+			continue
+		}
+
+		// symtab info
+		file, line := fn.FileLine(pc)
+		name := fn.Name()
+
+		// trim compile time GOPATH from file name
+		fileWImportPath := trimGOPATH(name, file)
+
+		// Strip only method name from FQN
+		idx := strings.LastIndex(name, "/")
+		name = name[idx+1:]
+		idx = strings.Index(name, ".")
+		name = name[idx+1:]
+
+		rs[i] = rollbar.Frame{
+			Filename: fileWImportPath,
+			Method:   name,
+			Line:     line,
+		}
+	}
+
+	return rs
+}
+
+// Taken AS IS from errors pkg since it's not exported there.
+// Check out the source code with good comments on https://github.com/pkg/errors/blob/master/stack.go
+func trimGOPATH(name, file string) string {
+	const sep = "/"
+	goal := strings.Count(name, sep) + 2
+	i := len(file)
+	for n := 0; n < goal; n++ {
+		i = strings.LastIndex(file[:i], sep)
+		if i == -1 {
+			i = -len(sep)
+			break
+		}
+	}
+	file = file[i+len(sep):]
+	return file
+}
+
 // Handle all errors
 func ErrorHandlingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -83,8 +145,10 @@ func ErrorHandlingMiddleware() gin.HandlerFunc {
 			for _, e := range c.Errors {
 				switch e.Type {
 				case gin.ErrorTypePublic:
-					log.Warnf("Public error: %s", e.Error())
-					c.JSON(c.Writer.Status(), gin.H{"status": "error", "error": e.Error()})
+					if e.Err != nil {
+						log.Warnf("Public error: %s", e.Error())
+						c.JSON(c.Writer.Status(), gin.H{"status": "error", "error": e.Error()})
+					}
 
 				case gin.ErrorTypeBind:
 					// Keep the preset response status
@@ -119,8 +183,19 @@ func ErrorHandlingMiddleware() gin.HandlerFunc {
 				default:
 					// Log all other errors
 					log.Error(e.Err)
+					st, ok := e.Err.(stackTracer)
+					if ok {
+						fmt.Printf("%s: %+v\n", st, st.StackTrace())
+					}
+
+					// Log to rollbar if we have a token setup
 					if len(rollbar.Token) != 0 {
-						rollbar.RequestError(rollbar.ERR, c.Request, e.Err)
+						if ok {
+							rollbar.RequestErrorWithStack(rollbar.ERR, c.Request, e.Err,
+								errorsToRollbarStack(st))
+						} else {
+							rollbar.RequestError(rollbar.ERR, c.Request, e.Err)
+						}
 					}
 				}
 			}
