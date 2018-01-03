a4dd692626239e110d294061742d36536a15378e
bbfsdev <bbfsdev@gmail.com>
2018-01-03 03:31:07 +0000

    Interface to incrementaly update index.

---
+++ es/indexer.go
@@ -0,0 +1,72 @@
+package es
+
+// Set of MDB event handlers to incrementally change all indexes.
+
+func CollectionAdd(uid string) error {
+    return nil
+}
+
+func CollectionUpdate(uid string) error {
+    return nil
+}
+
+func CollectionDelete(uid string) error {
+    return nil
+}
+
+func ContentUnitAdd(uid string) error {
+    return nil
+}
+
+func ContentUnitUpdate(uid string) error {
+    return nil
+}
+
+func ContentUnitDelete(uid string) error {
+    return nil
+}
+
+func FileAdd(uid string) error {
+    return nil
+}
+
+func FileUpdate(uid string) error {
+    return nil
+}
+
+func FileDelete(uid string) error {
+    return nil
+}
+
+func SourceAdd(uid string) error {
+    return nil
+}
+
+func SourceUpdate(uid string) error {
+    return nil
+}
+
+func TagAdd(uid string) error {
+    return nil
+}
+
+func TagUpdate(uid string) error {
+    return nil
+}
+
+func PersonAdd(uid string) error {
+    return nil
+}
+
+func PersonUpdate(uid string) error {
+    return nil
+}
+
+func PublisherAdd(uid string) error {
+    return nil
+}
+
+func PublisherUpdate(uid string) error {
+    return nil
+}
+
