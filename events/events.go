ec4634deb7c9e235a63b45533f3a7695cedcb9e3
yosefy <yosef.yudilevich@gmail.com>
2018-01-08 05:27:22 +0200

    prints uids from the bus

--- events/events.go
+++ events/events.go
@@ -6,35 +6,38 @@ import (
 	"os"
 	"os/signal"
 
-
 	"github.com/nats-io/go-nats-streaming"
 	"github.com/spf13/viper"
-
+	"encoding/json"
 )
 
-
 func printMsg(m *stan.Msg, i int) {
-	log.Printf("[#%d] Received on [%s]: '%s'\n", i, m.Subject, m)
+	log.Printf("[#%d] Received on [%s]: '%v'\n", i, m.Subject, m.Data)
+}
+
+// struct for the messages from bus
+type Payload struct {
+	Id int64 `json:"id"`
+	Uid	string `json:"uid"`
+}
+
+type Data struct {
+	Id string `json:"id"`
+	Type string `json:"type"`
+	Payload Payload `json:"payload"`
 }
 
 
 
 
+//RunLListener runs listener for nats server and reacts to new events
 func RunLListener() {
 
-
-
+	//
+	//
+	//
 	// TODO:
-	// 1. read from config and setup connctions to:
-	// MDB
-	// ES
-	// NATS streaming server
-	// config for assets api
-
-	// First task - connect to nats and print all events to log
-	// connect to nats
-
-	// Unfortunately, there is an open issue regarding connection failures on startup.
+	// run Kolman's stuff
 	// see https://github.com/nats-io/go-nats/issues/195
 	// we should upgrade as soon as it's fixed !
 
@@ -43,27 +46,36 @@ func RunLListener() {
 	natsClusterId := viper.GetString("nats.cluster-id")
 	natsSubject := viper.GetString("nats.subject")
 
+
+	// connect to nats server
 	sc, err := stan.Connect(natsClusterId, natsClientId, stan.NatsURL(natsUrl))
 	if err != nil {
 		log.Fatalf("Can't connect: %v.\nMake sure a NATS Streaming Server is running at: %s", err, natsUrl)
 	}
 
-
 	log.Printf("Connected to %s clusterID: [%s] clientID: [%s]\n", natsUrl, natsClusterId, natsClientId)
 
-	subj := natsSubject
-	i := 0
 
-	mcb := func(msg *stan.Msg) {
-		i++
-		printMsg(msg, i)
-	}
+	//mcb := func(msg *stan.Msg) {
+	//	var b Data
+	//
+	//	err := json.Unmarshal(msg.Data, &b)
+	//	if err != nil {
+	//		fmt.Printf("json.Unmarshal error: %s\n",err)
+	//	}
+	//	fmt.Printf("%v\n",b.Payload.Uid)
+	//
+	//}
 
 	startOpt := stan.DeliverAllAvailable()
 
-	sc.QueueSubscribe(subj, "", mcb, startOpt)
+	sc.QueueSubscribe(natsSubject, "", msgHandler, startOpt)
 
-	log.Printf("Listening on [%s], clientID=[%s], qgroup=[%s] durable=[%s]\n", subj, natsUrl)
+	log.Printf("Listening on [%s], clientID=[%s], qgroup=[%s] durable=[%s]\n", natsSubject, natsUrl)
+
+
+
+
 
 
 
@@ -80,4 +92,16 @@ func RunLListener() {
 		}
 	}()
 	<-cleanupDone
+}
+
+
+func msgHandler(msg *stan.Msg) {
+		var b Data
+
+		err := json.Unmarshal(msg.Data, &b)
+		if err != nil {
+			fmt.Printf("json.Unmarshal error: %s\n",err)
+		}
+		fmt.Printf("%v\n",b.Payload.Uid)
+
 }
