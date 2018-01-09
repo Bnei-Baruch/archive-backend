2a8c1366ef322d5455b11f7da997afeba808d286
yosefy <yosef.yudilevich@gmail.com>
2018-01-09 05:53:00 +0200

    add PersonDelete to indexer.go and format

--- es/indexer.go
+++ es/indexer.go
@@ -3,70 +3,73 @@ package es
 // Set of MDB event handlers to incrementally change all indexes.
 
 func CollectionAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func CollectionUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func CollectionDelete(uid string) error {
-    return nil
+	return nil
 }
 
 func ContentUnitAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func ContentUnitUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func ContentUnitDelete(uid string) error {
-    return nil
+	return nil
 }
 
 func FileAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func FileUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func FileDelete(uid string) error {
-    return nil
+	return nil
 }
 
 func SourceAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func SourceUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func TagAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func TagUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func PersonAdd(uid string) error {
-    return nil
+	return nil
+}
+
+func PersonDelete(uid string) error {
+	return nil
 }
 
 func PersonUpdate(uid string) error {
-    return nil
+	return nil
 }
 
 func PublisherAdd(uid string) error {
-    return nil
+	return nil
 }
 
 func PublisherUpdate(uid string) error {
-    return nil
+	return nil
 }
-
