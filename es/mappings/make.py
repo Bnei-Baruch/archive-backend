81f273d4ae9991667d83f051e974e07edec87b3f
LAPTOP-6SLSD0V3\Yuri <yurihechter@gmail.com>
2018-03-19 05:15:00 +0200

    sources template + using path.join to support all OS's

--- es/mappings/make.py
+++ es/mappings/make.py
@@ -16,6 +16,7 @@
 #   sudo bin/elasticsearch-plugin install analysis-icu
 
 import json
+import os
 
 # List of all languages
 ENGLISH = "en"
@@ -392,6 +393,93 @@ COLLECTIONS_TEMPLATE = {
   },
 }
 
+SOURCES_TEMPLATE = {
+  "settings": {
+    "index": {
+      "analysis": {
+        "analyzer": {
+          "phonetic_analyzer": {
+            "tokenizer": "standard",
+            "filter": [
+              "standard",
+              "lowercase",
+              lambda lang: IsCyrillic(lang, 'icu_transliterate'),
+              "custom_phonetic",
+            ],
+          },
+        },
+        "filter": {
+          "icu_transliterate": lambda lang: IsCyrillic(lang, {
+            "type": "icu_transform",
+            "id": "Any-Latin; NFD; [:Nonspacing Mark:] Remove; NFC",
+          }),
+          "custom_phonetic": {
+            "type": "phonetic",
+            "encoder": "beider_morse",
+            "replace": True,
+            "languageset": BeiderMorseLanguageset,
+          },
+        },
+      },
+    },
+  },
+  "mappings": {
+    "sources": {
+      "_all": {
+        "enabled": False,
+      },
+      "properties": {
+        "mdb_uid": {
+          "type": "keyword",
+        },
+        "name": {
+          "type": "text",
+          "analyzer": "phonetic_analyzer",
+          "fields": {
+            "analyzed": {
+              "type": "text",
+              "analyzer": lambda lang: StandardAnalyzer[lang],
+            },
+          },
+        },
+        "description": {
+          "type": "text",
+          "analyzer": "phonetic_analyzer",
+          "fields": {
+            "analyzed": {
+              "type": "text",
+              "analyzer": lambda lang: StandardAnalyzer[lang],
+            },
+          },
+        },
+        "original_language": {
+          "type": "keyword",
+          "index": False,
+        },
+        "translations": {
+          "type": "keyword",
+          "index": False,
+        },
+        "authors": {
+          "type": "keyword",
+          "index": False,
+        },
+        "content": {
+          "type": "text",
+          "analyzer": "phonetic_analyzer",
+          "fields": {
+            "analyzed": {
+              "type": "text",
+              "analyzer": lambda lang: StandardAnalyzer[lang],
+            }
+          },
+        },
+      },
+    },
+  },
+}
+
+
 def Resolve(lang, value):
   if isinstance(value, dict):
     l = [(k, Resolve(lang, v)) for (k, v) in value.iteritems()]
@@ -405,9 +493,11 @@ def Resolve(lang, value):
 
 
 for lang in LANG_GROUPS[ALL]:
-  with open('./data/es/mappings/units/units-%s.json' % lang, 'w') as f:
+  with open(os.path.join('.', 'data', 'es', 'mappings', 'units', 'unit-%s.json' % lang), 'w') as f:
     json.dump(Resolve(lang, UNITS_TEMPLATE), f, indent=4)
-  with open('./data/es/mappings/classifications/classifications-%s.json' % lang, 'w') as f:
+  with open(os.path.join('.', 'data', 'es', 'mappings', 'classifications', 'classifications-%s.json' % lang), 'w') as f:
     json.dump(Resolve(lang, CLASSIFICATIONS_TEMPLATE), f, indent=4)
-  with open('./data/es/mappings/collections/collections-%s.json' % lang, 'w') as f:
+  with open(os.path.join('.', 'data', 'es', 'mappings', 'collections', 'collection-%s.json' % lang), 'w') as f:
     json.dump(Resolve(lang, COLLECTIONS_TEMPLATE), f, indent=4)
+  with open(os.path.join('.', 'data', 'es', 'mappings', 'sources', 'sources-%s.json' % lang), 'w') as f:
+    json.dump(Resolve(lang, SOURCES_TEMPLATE), f, indent=4)
