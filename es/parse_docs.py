11bedd55218feeac1a1537c24468c2cc5ac67d1a
bbfsdev <bbfsdev@gmail.com>
2018-05-03 13:48:02 +0000

    Fix parse docx to be efficient. Still don't add not needed newlines as this is not convinient for tests.

--- es/parse_docs.py
+++ es/parse_docs.py
@@ -5,6 +5,11 @@ from docx import Document
 
 with open(sys.argv[1], 'rb') as f:
     document = Document(f)
-    text = '\n'.join([p.text.encode('utf-8') for p in document.paragraphs])
-    sys.stdout.write(text)
-    sys.stdout.flush()
+    first = True
+    for p in document.paragraphs:
+        if not first:
+            sys.stdout.write('\n')
+        sys.stdout.write(p.text.encode('utf-8'))
+        sys.stdout.flush()
+        if first:
+            first = False
