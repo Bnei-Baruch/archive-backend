832b92d5af964c7b5a290b64d84e356c09422ace
Edo Shor <edoshor@gmail.com>
2018-06-22 04:43:04 +0300

    RSS Feeds - fix extension case

--- api/routes.go
+++ api/routes.go
@@ -27,10 +27,14 @@ func SetupRoutes(router *gin.Engine) {
 	router.GET("/sqdata", SemiQuasiDataHandler)
 	router.GET("/stats/cu_class", StatsCUClassHandler)
 
+	router.GET("/feeds/rus_zohar", FeedRusZohar)
 	router.GET("/feeds/rus_zohar.rss", FeedRusZohar)
+	router.GET("/feeds/rus_for_laitman_ru", FeedRusForLaitmanRu)
 	router.GET("/feeds/rus_for_laitman_ru.rss", FeedRusForLaitmanRu)
+	router.GET("/feeds/morning_lesson", FeedMorningLesson)
 	router.GET("/feeds/morning_lesson.rss", FeedMorningLesson)
 	router.GET("/feeds/rss.php", FeedRssPhp)
+	router.GET("/feeds/podcast", FeedPodcast)
 	router.GET("/feeds/podcast.rss", FeedPodcast)
 	router.GET("/feeds/wsxml", FeedWSXML)
 
