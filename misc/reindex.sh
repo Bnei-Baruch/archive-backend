bc7d933907cb4f3b82bd7b807604cf9336414bf4
Edo Shor <edoshor@gmail.com>
2018-06-27 18:06:08 +0300

    misc - improvements to reindex script

--- misc/reindex.sh
+++ misc/reindex.sh
@@ -8,14 +8,18 @@ TIMESTAMP="$(date '+%Y%m%d%H%M%S')"
 LOG_FILE="$BASE_DIR/logs/es/reindex_$TIMESTAMP.log"
 
 cd ${BASE_DIR}
-./archive-backend convert-docx > ${LOG_FILE} 2>&1
-./archive-backend index classifications > ${LOG_FILE} 2>&1
-./archive-backend index collections > ${LOG_FILE} 2>&1
-./archive-backend index sources > ${LOG_FILE} 2>&1
-./archive-backend index units > ${LOG_FILE} 2>&1
+./archive-backend convert-docx >> ${LOG_FILE} 2>&1
+
+supervisorctl stop events
+
+./archive-backend index classifications >> ${LOG_FILE} 2>&1
+./archive-backend index collections >> ${LOG_FILE} 2>&1
+./archive-backend index sources >> ${LOG_FILE} 2>&1
+./archive-backend index units >> ${LOG_FILE} 2>&1
 
 curl -X POST "elastic.mdb.local:9200/_refresh"
 
+supervisorctl start events
 
 WARNINGS="$(egrep -c "level=(warning|error)" ${LOG_FILE})"
 
@@ -26,4 +30,4 @@ fi
 
 echo "Errors in periodic import of storage catalog to MDB" | mail -s "ERROR: ES reindex" -r "mdb@bbdomain.org" -a ${LOG_FILE} edoshor@gmail.com
 
-find "${BASE_DIR}/logs/es/reindex_*.log" -type f -mtime +7 -exec rm -f {} \;
+find ${BASE_DIR}/logs/es -name "reindex_*.log" -type f -mtime +7 -exec rm -f {} \;
