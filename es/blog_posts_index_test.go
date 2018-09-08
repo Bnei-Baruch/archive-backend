60652d2d7326f5a1e0cc3f46f24c50f4c55d266a
LAPTOP-NFLD56CB\Yuri <yurihechter@gmai.com>
2018-09-08 12:19:33 +0300

    blog index - tests expand

--- es/blog_posts_index_test.go
+++ es/blog_posts_index_test.go
@@ -60,8 +60,18 @@ func (suite *BlogIndexerSuite) TestBlogIndex() {
 	r.Nil(indexer.BlogPostUpdate(id5))
 	suite.validateNames(indexNameEn, indexer, []string{"this is english post"})
 
+	fmt.Println("\nAdding another english post and validate.")
+	id6 := suite.ibp(6, 2, "this is another english post", false)
+	r.Nil(indexer.BlogPostUpdate(id6))
+	suite.validateNames(indexNameEn, indexer, []string{"this is english post", "this is another english post"})
+
+	fmt.Println("\nDelete first english post and validate.")
+	r.Nil(deletePosts([]string{id1}))
+	r.Nil(indexer.BlogPostUpdate(id1))
+	suite.validateNames(indexNameEn, indexer, []string{"this is another english post"})
+
 	fmt.Println("\nDelete posts from DB, reindex and validate we have 0 posts.")
-	r.Nil(deletePosts([]string{id1, id2, id3, id4}))
+	r.Nil(deletePosts([]string{id2, id3, id4, id6}))
 	r.Nil(indexer.ReindexAll())
 	suite.validateNames(indexNameEn, indexer, []string{})
 	suite.validateNames(indexNameEs, indexer, []string{})
