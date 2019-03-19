8a2464d980edc77b3d75568f5195c3d8ed1356cb
LAPTOP-6SLSD0V3\Yuri <yurihechter@gmail.com>
2019-03-19 06:28:28 +0200

    time log map with mutex

--- search/search_logger.go
+++ search/search_logger.go
@@ -115,11 +115,11 @@ func (searchLogger *SearchLogger) LogClick(mdbUid string, index string, resultTy
 	return nil
 }
 
-func (searchLogger *SearchLogger) LogSearch(query Query, sortBy string, from int, size int, searchId string, suggestion string, res *QueryResult, executionTimeLog map[string]time.Duration) error {
+func (searchLogger *SearchLogger) LogSearch(query Query, sortBy string, from int, size int, searchId string, suggestion string, res *QueryResult, executionTimeLog *TimeLogMap) error {
 	return searchLogger.logSearch(query, sortBy, from, size, searchId, suggestion, res, nil, executionTimeLog)
 }
 
-func (searchLogger *SearchLogger) LogSearchError(query Query, sortBy string, from int, size int, searchId string, suggestion string, searchErr interface{}, executionTimeLog map[string]time.Duration) error {
+func (searchLogger *SearchLogger) LogSearchError(query Query, sortBy string, from int, size int, searchId string, suggestion string, searchErr interface{}, executionTimeLog *TimeLogMap) error {
 	return searchLogger.logSearch(query, sortBy, from, size, searchId, suggestion, nil, searchErr, executionTimeLog)
 }
 
@@ -156,11 +156,11 @@ func (searchLogger *SearchLogger) fixResults(res *QueryResult) *QueryResult {
 	return res
 }
 
-func (searchLogger *SearchLogger) logSearch(query Query, sortBy string, from int, size int, searchId string, suggestion string, res *QueryResult, searchErr interface{}, executionTimeLog map[string]time.Duration) error {
+func (searchLogger *SearchLogger) logSearch(query Query, sortBy string, from int, size int, searchId string, suggestion string, res *QueryResult, searchErr interface{}, executionTimeLog *TimeLogMap) error {
 
 	timeLogArr := []TimeLog{}
-	for k := range executionTimeLog {
-		ms := int64(executionTimeLog[k] / time.Millisecond)
+	for k, v := range executionTimeLog.ToMap() {
+		ms := int64(v / time.Millisecond)
 		timeLogArr = append(timeLogArr, TimeLog{Operation: k, Time: ms})
 	}
 
