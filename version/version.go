e9ce5847a0aadfaa13dd35101225ea00b7d1cf5f
bbfsdev <bbfsdev@gmail.com>
2019-06-21 11:39:17 +0000

    Update version.

--- version/version.go
+++ version/version.go
@@ -5,8 +5,8 @@ import "fmt"
 // See http://semver.org/ for more information on Semantic Versioning
 var (
 	Major      = 1
-	Minor      = 4
-	Patch      = 9
+	Minor      = 5
+	Patch      = 0
 	PreRelease = "dev"
 )
 
