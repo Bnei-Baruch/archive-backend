09ca0e987ebcb409ae3a22927d3f721f9ac177eb
LAPTOP-6SLSD0V3\Yuri <yurihechter@gmail.com>
2019-07-26 05:38:39 +0300

    remove lesson series from black list

--- es/collections_index.go
+++ es/collections_index.go
@@ -35,11 +35,10 @@ type CollectionsIndex struct {
 }
 
 func defaultCollectionsSql() string {
-	return fmt.Sprintf("c.secure = 0 AND c.published IS TRUE AND c.type_id NOT IN (%d, %d, %d, %d, %d, %d, %d)",
+	return fmt.Sprintf("c.secure = 0 AND c.published IS TRUE AND c.type_id NOT IN (%d, %d, %d, %d, %d, %d)",
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_DAILY_LESSON].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_SPECIAL_LESSON].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_CLIPS].ID,
-		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_LESSONS_SERIES].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_SONGS].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_BOOKS].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_UNKNOWN].ID,
