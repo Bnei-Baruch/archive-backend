f420a356a73d5076135888c1993dd0ca160afc78
Edo Shor <edoshor@gmail.com>
2019-10-15 05:53:46 +0300

    CMS - fix duplicate nested dirs + code style

--- cms/cms.go
+++ cms/cms.go
@@ -26,31 +26,30 @@ func SyncCMS() {
 
 	cms := viper.GetString("cms.url")
 	assets := viper.GetString("cms.assets")
-
-	passive, err := prepareDirectories(assets)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	log.Info("Source URL: ", cms)
-	log.Info("Target directory: ", assets)
+	log.Info("Assets directory: ", assets)
+
+	workDir, err := prepareDirectories(assets)
+	if err != nil {
+		log.Fatal("prepare directories", err)
+	}
+	log.Info("Work directory: ", workDir)
 
 	log.Info("Syncing Banners...")
-	syncBanners(cms, passive)
-	log.Info("Done")
+	syncBanners(cms, workDir)
 
 	log.Info("Syncing Persons...")
-	syncPersons(cms, passive)
-	log.Info("Done")
+	syncPersons(cms, workDir)
 
 	// log.Info("Syncing Sources...")
-	// syncSources(cms, passive)
+	// syncSources(cms, workDir)
 	// log.Info("Done")
 
 	log.Info("Switching Directories...")
-	if err = switchDirectories(assets, passive); err != nil {
-		log.Fatal(err)
+	if err = switchDirectories(assets, workDir); err != nil {
+		log.Fatal("switch directories", err)
 	}
+
 	log.Info("Done")
 }
 
@@ -70,7 +69,7 @@ func syncPersons(cms string, assets string) {
 	var err error
 
 	if err = getItem("persons", cms+"get-persons/all", &persons); err != nil {
-		log.Fatal(err)
+		log.Fatal("get persons", err)
 	}
 
 	for _, person := range persons {
@@ -78,80 +77,84 @@ func syncPersons(cms string, assets string) {
 		if err = checkSlug4Language(person.Slug, personsLanguage); err != nil {
 			log.Fatal(err)
 		}
-		if err = saveItem("person", filepath.Join(assets, "persons", person.Slug), person); err != nil {
-			log.Fatal(err)
+		if err = saveItem(filepath.Join(assets, "persons", person.Slug), person); err != nil {
+			log.Fatal("save person", err)
 		}
 	}
 }
 
-func syncBanners(cms string, assets string) {
+func syncBanners(cms string, workDir string) {
 	var banners []item
 	var err error
 
-	imageURL := viper.GetString("cms.image-url")
-
 	if err = getItem("banners", cms+"get-banners/all", &banners); err != nil {
-		log.Fatal(err)
+		log.Fatal("get banners", err)
 	}
 
+	imageURL := viper.GetString("cms.image-url")
 	for _, banner := range banners {
 		log.Info(banner.Slug)
 		if err = checkSlug4Language(banner.Slug, bannersLanguage); err != nil {
 			log.Fatal(err)
 		}
 
-		if err = saveItem("banners", filepath.Join(assets, "banners", banner.Slug), banner); err != nil {
-			log.Fatal(err)
+		if err = saveItem(filepath.Join(workDir, "banners", banner.Slug), banner); err != nil {
+			log.Fatal("save banner", err)
 		}
 
 		// create directories for images
 		image := banner.Meta["image"]
-		if err = mkdir(0755, assets, "images", path.Dir(image)); err != nil {
-			log.Fatal(err)
+		if err = mkdir(0755, workDir, "images", path.Dir(image)); err != nil {
+			log.Fatal("make images dir", err)
 		}
-		if err = saveImage(image, imageURL, assets); err != nil {
-			log.Fatal(err)
+		if err = saveImage(image, imageURL, workDir); err != nil {
+			log.Fatal("save image", err)
 		}
 	}
 }
 
 func mkdir(permissions os.FileMode, dirs ...string) (err error) {
 	dirname := filepath.Join(dirs...)
-	info, err := os.Stat(dirname)
+	_, err = os.Stat(dirname)
 	if os.IsNotExist(err) {
 		if err = os.MkdirAll(dirname, permissions); err != nil {
-			return errors.Wrapf(err, "Unable to create directory: %s", dirname)
+			return errors.Wrapf(err, "os.MkdirAll: %s", dirname)
 		}
-	} else if info.Mode().IsRegular() {
-		return errors.Wrapf(err, "Directory already exists as a file: %s", dirname)
+	} else {
+		err = errors.Wrapf(err,"os.Stat: %s", dirname)
 	}
 	return
 }
 
 /* Create passive directory */
-func prepareDirectories(assets string) (inactive string, err error) {
-	t := time.Now().Unix()
-	inactive = filepath.Join(assets, fmt.Sprint(t))
-	_, err = os.Stat(inactive)
-	if err = mkdir(0755, assets, inactive, "banners"); err != nil {
-		return "", errors.Wrapf(err, "Unable to create directory for banners: %s/banners", inactive)
+func prepareDirectories(assets string) (workDir string, err error) {
+	workDir = filepath.Join(assets, fmt.Sprint(time.Now().Unix()))
+
+	if err = mkdir(0755, workDir, "banners"); err != nil {
+		return "", errors.Wrapf(err, "mkdir banners: %s/banners", workDir)
 	}
-	if err = mkdir(0755, assets, inactive, "persons"); err != nil {
-		return "", errors.Wrapf(err, "Unable to create directory for persons: %s/persons", inactive)
+
+	if err = mkdir(0755, workDir, "persons"); err != nil {
+		return "", errors.Wrapf(err, "mkdir persons: %s/persons", workDir)
 	}
-	return inactive, nil
+
+	return workDir, nil
 }
 
-func switchDirectories(assets, inactive string) (err error) {
+func switchDirectories(assets, workDir string) (err error) {
 	var active = filepath.Join(assets, "active")
 	_, err = os.Stat(active)
 	if os.IsExist(err) {
 		if err = os.Remove(active); err != nil {
-			return errors.Wrapf(err, "Unable to remove link %s", active)
+			return errors.Wrapf(err, " os.Remove: %s", active)
 		}
 	}
-	if err = os.Symlink(inactive, active); err != nil {
-		return errors.Wrapf(err, "Unable to create link %s to %s", active, inactive)
+	absWorkDir, err := filepath.Abs(workDir)
+	if err != nil {
+		return errors.Wrapf(err, "filepath.Abs: %s", workDir)
+	}
+	if err = os.Symlink(absWorkDir, active); err != nil {
+		return errors.Wrapf(err, "os.Symlink: %s to %s", active, absWorkDir)
 	}
 	return
 }
@@ -165,14 +168,14 @@ func checkSlug4Language(slug string, pattern *regexp.Regexp) (err error) {
 	return
 }
 
-func saveItem(name string, path string, v interface{}) (err error) {
+func saveItem(path string, v interface{}) (err error) {
 	m, err := json.Marshal(v)
 	if err != nil {
-		return errors.Wrapf(err, "saveItem::Marshal %s", name)
+		return errors.Wrapf(err, "json.Marshal: %s", path)
 	}
 	err = ioutil.WriteFile(path, m, 0644)
 	if err != nil {
-		return errors.Wrapf(err, "saveItem::WriteFile %s", name)
+		return errors.Wrapf(err, "ioutil.WriteFile: %s", path)
 	}
 	return
 }
@@ -180,44 +183,44 @@ func saveItem(name string, path string, v interface{}) (err error) {
 func getItem(name string, url string, v interface{}) (err error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return errors.Wrapf(err, "getItem::NewRequest prepare %s", name)
+		return errors.Wrapf(err, "http.NewRequest: %s", name)
 	}
 	res, err := client.Do(req)
 	if err != nil {
-		return errors.Wrapf(err, "getItem::Do GET %s", name)
+		return errors.Wrapf(err, "client.Do: %s", name)
 	}
 	defer func() {
 		x := res.Body.Close()
 		if x != nil {
-			err = errors.Wrapf(x, "getItem::Close close body %s", name)
+			err = errors.Wrapf(x, "res.Body.Close: %s", name)
 			log.Fatal(err)
 		}
 	}()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return errors.Wrapf(err, "getItem::ReadAll read body %s", name)
+		return errors.Wrapf(err, "ioutil.ReadAll: %s", name)
 	}
 	err = json.Unmarshal(body, v)
 	if err != nil {
-		return errors.Wrapf(err, "getItem::Unmarshal unmarshal %s", name)
+		return errors.Wrapf(err, "json.Unmarshal: %s", name)
 	}
 	return
 }
 
-func saveImage(image string, imageURL string, assets string) (err error) {
+func saveImage(image string, imageURL string, workDir string) (err error) {
 	// copy images
 	res, err := http.Get(imageURL + image)
 	if err != nil {
-		return errors.Wrapf(err, "saveImage::Get %s", image)
+		return errors.Wrapf(err, "http.Get %s", image)
 	}
-	out, err := os.Create(filepath.Join(assets, "images", image))
+	out, err := os.Create(filepath.Join(workDir, "images", image))
 	if err != nil {
-		return errors.Wrapf(err, "saveImage::Create %s", image)
+		return errors.Wrapf(err, "os.Create %s", image)
 	}
 	// Write the body to file
 	if _, err = io.Copy(out, res.Body); err != nil {
-		return errors.Wrapf(err, "saveImage::Copy %s", image)
+		return errors.Wrapf(err, "io.Copy %s", image)
 	}
 	defer func() {
 		x := res.Body.Close()
@@ -227,7 +230,7 @@ func saveImage(image string, imageURL string, assets string) (err error) {
 		}
 	}()
 	if err = out.Close(); err != nil {
-		return errors.Wrapf(err, "saveImage::Close %s", image)
+		return errors.Wrapf(err, "out.Close %s", image)
 	}
 	return
 }
