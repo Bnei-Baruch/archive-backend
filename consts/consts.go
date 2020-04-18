48c462c4a473e38370024b1585717330541ec174
LAPTOP-NFLD56CB\Yuri <yurihechter@gmai.com>
2020-04-18 05:39:48 +0300

    new highlight timeout

--- consts/consts.go
+++ consts/consts.go
@@ -304,7 +304,7 @@ const (
 	API_DEFAULT_PAGE_SIZE                     = 50
 	API_MAX_PAGE_SIZE                         = 1000
 	MIN_RESULTS_SCORE_TO_IGNOGRE_TYPO_SUGGEST = 100
-	TIMEOUT_FOR_HIGHLIGHT_SEARCH              = 4500
+	TIMEOUT_FOR_HIGHLIGHT_SEARCH              = 15000
 	// Consider makeing a carusele and not limiting.
 	MAX_MATCHES_PER_GRAMMAR_INTENT = 3
 )
