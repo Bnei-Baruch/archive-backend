7310503fbb5813a58bedc22145a8a0ba964168ba
davgur <gur28davaravut>
2020-07-14 05:43:20 +0300

    add comment

--- search/quality_eval.go
+++ search/quality_eval.go
@@ -698,6 +698,7 @@ func EvaluateQuery(q EvalQuery, serverUrl string, skipExpectations bool) EvalRes
 					}
 					if HitMatchesExpectation(hit, hitSource, q.Expectations[i]) {
 						rank = j + 1
+						//set good if first 3 are till 3 place in results, or 4 and 5 are till 5 place
 						if j <= 2 || (i > 2 && j <= 4) {
 							sq = SQ_GOOD
 						} else {
