141f9acbbde83a627f587dc4ebe13b737f89611d
LAPTOP-NFLD56CB\Yuri <yurihechter@gmai.com>
2020-10-22 06:26:42 +0300

    version incr.

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // See http://semver.org/ for more information on Semantic Versioning
 var (
 	Major      = 1
-	Minor      = 9
+	Minor      = 10
 	Patch      = 0
 	PreRelease = "dev"
 )
