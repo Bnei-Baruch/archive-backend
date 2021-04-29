d8243b6b603c8846ac32130f6e2e07ffcde7d292
LAPTOP-NFLD56CB\Yuri <yurihechter@gmai.com>
2021-04-29 20:47:20 +0300

    comment typo

--- search/models.go
+++ search/models.go
@@ -49,7 +49,7 @@ type SearchRequestOptions struct {
 	titlesOnly bool
 	// Setting the following field to 'true' will include 'typed_uids' values for results of type 'content units'.
 	// We use this data for a further filtering out of hits recieved from 'grammar filter' search that duplicates the carousel items.
-	// Since the search for 'grammar filter' is async. to  classification intents (carousel) search, we don't have yet the data for filterOutCUSources fild.
+	// Since the search for 'grammar filter' is async. to  classification intents (carousel) search, we don't have yet the data for filterOutCUSources field.
 	includeTypedUidsFromContentUnits bool
 	// If not nil, set how long a search is allowed to take, e.g. "1s" or "500ms". Note: Not always respected by ES.
 	Timeout *string
