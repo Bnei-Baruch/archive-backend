c2dfa34181438e26e174afedcc099354da8ea43b
davgur <davgur.ru@gmail.com>
2021-05-31 10:43:01 +0300

    fix exceptions on tests

--- es/content_units_index.go
+++ es/content_units_index.go
@@ -51,7 +51,7 @@ func defaultContentUnit(cu *mdbmodels.ContentUnit) bool {
 }
 
 func defaultContentUnitSql() string {
-	return fmt.Sprintf("cu.secure = 0 AND cu.published IS TRUE AND cu.type_id NOT IN (%d, %d, %d, %d, %d, %d, %d, %d, %d)",
+	return fmt.Sprintf("cu.secure = 0 AND cu.published IS TRUE AND cu.type_id NOT IN (%d, %d, %d, %d, %d, %d, %d, %d, %d, %d)",
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_LELO_MIKUD].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_PUBLICATION].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_SONG].ID,
