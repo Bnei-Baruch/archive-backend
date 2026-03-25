1715ff598cf218e9aa3ab6de1b95946ea048e5ea
Yuri Hechter <yurihechter@gmail.com>
2026-03-25 08:19:50 +0200

    allow CT_LIKUTIM index

--- es/content_units_index.go
+++ es/content_units_index.go
@@ -55,7 +55,9 @@ func defaultContentUnit(cu *mdbmodels.ContentUnit) bool {
 }
 
 func defaultContentUnitSql() string {
-	return fmt.Sprintf("cu.secure = 0 AND cu.published IS TRUE AND cu.type_id NOT IN (%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)",
+	// Note that CT_LIKUTIM is not filtered out and will be indexed,
+	// but it is not supported by the search (only optional in the elasticsearch_search agent tool).
+	return fmt.Sprintf("cu.secure = 0 AND cu.published IS TRUE AND cu.type_id NOT IN (%d, %d, %d, %d, %d, %d, %d, %d, %d, %d)",
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_LELO_MIKUD].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_PUBLICATION].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_SONG].ID,
@@ -66,7 +68,6 @@ func defaultContentUnitSql() string {
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_KTAIM_NIVCHARIM].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_UNKNOWN].ID,
 		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_SOURCE].ID,
-		mdb.CONTENT_TYPE_REGISTRY.ByName[consts.CT_LIKUTIM].ID,
 	)
 }
 
